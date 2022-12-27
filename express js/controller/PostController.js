f3c2ae7a2f5c88abfc90423a6f363d4e6e5ab293
deserved2k <tulgaa.m2008@gmail.com>
2022-12-27 11:09:09 +0800

    siuuu

--- express js/controller/PostController.js
+++ express js/controller/PostController.js
@@ -1,38 +1,50 @@
-const Post= require("../models/Postman")
+const Post = require("../models/Postman");
 
-exports.getPosts = async( request,response)=>{
-    try{
-     const posts= await Post.find()
-     response.send(posts);
-    }catch(error){
-        response.status().send({message:error.message})
-    }
-}
-exports.getPostsbyId = async( request,response)=>{
-    const _id=request.params.id
-    try{
-        const posts= await Post.findById({_id})
-        response.send(posts)
-    }catch(error){
-        response.status().send({message:error.message})
-    }
-}
-exports.createPosts = async( request,response)=>{
-    const posts=request.body
-    try{
-    const PostData= await Post.create(posts);
-    response.send(PostData)
-    }catch(error){
-        response.status(500).send({message:"Failed to create"+ error.message})
-    }
-}
-exports.updatePosts = async( request,response)=>{
-    const posts=request.body
-    const _id=request.params.id
-    try{
-    const PostData= await Post.findByIdAndUpdate({_id},posts)
-    response.send(PostData)
-    }catch(error){
-        response.status(400).send({message:"Failed to update"+error.message})
-    }
-}
+exports.getPosts = async (request, response) => {
+  try {
+    const posts = await Post.find();
+    response.send(posts);
+  } catch (error) {
+    response.status().send({ message: error.message });
+  }
+};
+exports.getPostsbyId = async (request, response) => {
+  const _id = request.params.id;
+  try {
+    const posts = await Post.findById({ _id });
+    response.send(posts);
+  } catch (error) {
+    response.status().send({ message: error.message });
+  }
+};
+exports.createPosts = async (request, response) => {
+  const posts = request.body;
+  try {
+    const PostData = await Post.create(posts);
+    response.send(PostData);
+  } catch (error) {
+    response.status(500).send({ message: "Failed to create" + error.message });
+  }
+};
+exports.updatePosts = async (request, response) => {
+  const posts = request.body;
+  const _id = request.params.id;
+  try {
+    const PostData = await Post.findByIdAndUpdate({ _id }, posts);
+    response.send(PostData);
+  } catch (error) {
+    response.status(400).send({ message: "Failed to update" + error.message });
+  }
+};
+exports.deletePosts = async (request, response) => {
+  const _id = request.params.id;
+  const posts = request.body;
+  try {
+    const deletedPostData = await Post.findByIdAndDelete({ _id }, posts);
+    response.send("" + _id);
+  } catch (error) {
+    response
+      .status(400)
+      .send({ message: "Failed to delete" + "" + error.message });
+  }
+};
--- express js/router/PostRouter.js
+++ express js/router/PostRouter.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const router= express.Router();
-const{
-    getPosts,
-    createPosts,
-    getPostsbyId,
-    updatePosts,
-    // deletePosts,
-} =require("../controller/PostController");
+const router = express.Router();
+const {
+  getPosts,
+  createPosts,
+  getPostsbyId,
+  updatePosts,
+  deletePosts,
+} = require("../controller/PostController");
 
 router
-.get("/posts", getPosts)
-.get("/posts/:id",getPostsbyId)
-.post("/posts/create", createPosts)
-.put("/posts/:id", updatePosts)
-// .delete("/posts/:id", deletePosts)
+  .get("/posts", getPosts)
+  .get("/posts/:id", getPostsbyId)
+  .post("/posts/create", createPosts)
+  .put("/posts/:id", updatePosts)
+  .delete("/posts/:id", deletePosts);
 
-module.exports= router;
+module.exports = router;
